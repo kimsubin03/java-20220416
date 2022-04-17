ae698d75c694be0ee8ef2dfaf8352cf7eded0333
kimsubin03 <103809185+kimsubin03@users.noreply.github.com>
2022-04-17 14:32:34 +0900

    Add files via upload

---
+++ Practice4.java
@@ -0,0 +1,38 @@
+
+public class Practice4 {
+
+	public static void main(String[] args) {
+		//2+5=7
+		System.out.println("2+5=7");
+		
+		//포맷출력 ->줄바꿈X
+		//줄바꿈문자 ->\n
+		//%d->정수
+		//%s->문자열
+		//%f->실수
+		
+		int num1=2;
+		int num2=5;
+		
+		System.out.printf("2+5=%d\n",2+5);
+		System.out.printf("%d+%d=%d\n",num1,num2,num1+num2);
+		System.out.printf("aaa\n");
+		System.out.printf("aaa\n");
+		
+		//구구단 7단 작성
+		for(int i=1;i<10;i++) {
+		System.out.printf("%dX%d=%d\n",7,i,i*7);
+		}
+		
+		//홀수와 짝수를 구별해주세요.
+		// num이 짝수면 even, 홀수면 odd 출력해주세요.
+		// 짝수 판별 : 2로 나누어서 떨어지면 짝수 
+
+		
+		  }
+		
+
+		
+	}
+
+
