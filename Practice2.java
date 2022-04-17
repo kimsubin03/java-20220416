8929fca385b6ce99ba0be9a38db125fbe25116d3
kimsubin03 <103809185+kimsubin03@users.noreply.github.com>
2022-04-17 13:14:04 +0900

    Add files via upload

--- Practice2.java
+++ Practice2.java
@@ -5,8 +5,11 @@ public class Practice2 {
 		// TODO Auto-generated method stub
 		int age=10;
 		
+	
 		if(age<=13) {
-			System.out.println("초등");
+			if(age>6) {
+				System.out.println("초등");
+			}
 		}
 		else if(age<17) {
 			System.out.println("중등");
@@ -14,13 +17,16 @@ public class Practice2 {
 		else if(age<=19) {
 			System.out.println("고등");
 		}
-		else{
+		else if(age<25){
 			System.out.println("대학");
 			}
+		else {
+			System.out.println("학적 없음");
 		}
-		
-		
-
+		}
+		//자바에서 13<age<17 같은 사잇값 표현은 지원X
+	
+	
 	}
 
 
